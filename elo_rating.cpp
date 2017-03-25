cc9bf532941e6fba3044db27c638a6136585d995
emschenn <emilynov14@gmail.com>
2017-03-25 19:35:21 +0800

    add

---
+++ elo_rating.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+#include <math.h> 
+#include <fstream> 
+#include <cstdlib> 
+#include "elo_rating.h" 
+Data::Data() 
+{ 
+        ifstream inFile("infile",ios::in); 
+	if(!inFile) 
+        { 
+                cerr<<"Failed reading"<<endl; 
+                exit(1); 
+        } 
+        inFile>>k>>ra>>rb; 
+} 
+double Data::ea() 
+{ 
+        return(1/(1+pow(10,(rb-ra)/400))); 
+} 
+double Data::eb() 
+{ 
+        return(1/(1+pow(10,(ra-rb)/400))); 
+} 
+double Data::new_ra(double sa) 
+{ 
+        ra = ra+k*(sa-ea()); 
+        return ra; 
+} 
+double Data::new_rb(double sb) 
+{ 
+        rb = rb+k*(sb-eb()); 
+        return rb; 
+} 
